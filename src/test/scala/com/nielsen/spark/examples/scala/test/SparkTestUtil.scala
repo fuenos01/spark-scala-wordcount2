8765b87049107df3a0ddf9a57cd0f3c10615c367
Oswaldo Fuenmayor <Oswaldo.Fuenmayor@nielsen.com>
2018-02-16 11:03:26 -0500

    Minor code changes

--- src/test/scala/com/nielsen/spark/examples/scala/test/SparkTestUtil.scala
+++ src/test/scala/com/nielsen/spark/examples/scala/test/SparkTestUtil.scala
@@ -7,16 +7,13 @@ object SparkTestUtil {
   def initTestEnv() {
     val userDir: String = System.getProperty("user.dir").replace('\\', '/')
     val tmpDir: String = s"file:///$userDir/tmp"
-
     val sparkHiveWarehouseDir: String = s"file:///$userDir/tmp/spark-hive"
     val log4jFile: String = s"file:///$userDir/conf/log4j.properties"
 
     sys.props.put("hive.exec.scratchdir", s"$userDir/tmp")
     sys.props.put("spark.sql.warehouse.dir", sparkHiveWarehouseDir)
-
     sys.props.put("spark.local.dir", s"$userDir/tmp/spark/scratch")
     sys.props.put("spark.master", "local[*]")
-
     sys.props.put("log4j.configuration", log4jFile)
 
     deleteDirectory(s"$userDir/tmp")
